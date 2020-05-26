80067eb20cf3b5bda552bf31f5412959bb0db38e
biobdeveloper <ibogomolov321@gmail.com>
2020-05-26 03:55:02 +0300

    add close / to assets/ endpoint

--- routes.py
+++ routes.py
@@ -4,4 +4,4 @@ from config import API_V1_ADDRESS
 
 def init_routes(app) -> None:
     app.router.add_get('/', index)
-    app.router.add_get(f'{API_V1_ADDRESS}/assets', assets_view)
+    app.router.add_get(f'{API_V1_ADDRESS}/assets/', assets_view)
