27be80be797840a3b489c3c51023dfa4deb9a1bd
biobdeveloper <ibogomolov321@gmail.com>
2020-05-26 03:50:56 +0300

    Replace unittest to pytest and fix tests structure

--- tests/test_api.py
+++ tests/test_api.py
@@ -1,34 +1,32 @@
-from aiohttp.test_utils import AioHTTPTestCase, unittest_run_loop
+import pytest
+from aiohttp import web
+import sys
 
-from app import app_factory
+sys.path.append("/app")
+
 from db_utils import add_asset
 from config import PROJECT_NAME, API_V1_ADDRESS
-
+from gw_app import app_factory
 from tests.fixtures import test_assets_data
 
 
-class TestApi(AioHTTPTestCase):
-    async def get_application(self):
-        return app_factory()
+@pytest.fixture
+def cli(loop, aiohttp_client):
+    app = app_factory()
+    return loop.run_until_complete(aiohttp_client(app))
 
-    # Fill database with original API data
-    async def fill_test_data(self):
-        for asset_data in test_assets_data:
-            async with self.app['db'].acquire() as conn:
-                await add_asset(conn, **asset_data)
 
-    @unittest_run_loop
-    async def test_index(self):
-        resp = await self.client.request("GET", "/")
-        self.assertEqual(resp.status, 200)
-        text = await resp.text()
-        self.assertIn(PROJECT_NAME, text)
+async def test_index(cli):
+    resp = await cli.get('/')
+    assert resp.status == 200
+    assert PROJECT_NAME in await resp.text()
 
-    @unittest_run_loop
-    async def test_assets(self):
-        await self.fill_test_data()
-        resp = await self.client.request("GET", f"{API_V1_ADDRESS}/assets")
-        self.assertEqual(resp.status, 200)
-        json_data = await resp.json()
-        self.assertIsInstance(json_data, dict)
 
+async def test_assets(cli):
+    for asset_data in test_assets_data:
+        async with cli.server.app['db'].acquire() as conn:
+            await add_asset(conn, **asset_data)
+    resp = await cli.get(F"{API_V1_ADDRESS}/assets/")
+    assert resp.status == 200
+    json_data = await resp.json()
+    assert isinstance(json_data, dict)
