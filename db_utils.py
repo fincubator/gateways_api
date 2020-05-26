e5fd0eb752235bff7458a0aafdf072ed86543cdc
biobdeveloper <ibogomolov321@gmail.com>
2020-05-26 03:53:40 +0300

    Replace tables->models

--- db_utils.py
+++ db_utils.py
@@ -1,32 +1,24 @@
 import logging
 
 from aiopg.sa import SAConnection as SAConn
-
-from tables import asset
+from sqlalchemy.orm.query import Query
+from sqlalchemy.sql import insert
+from models import Asset
 
 
 logger = logging.getLogger(__name__)
 
 
 async def get_all_assets(conn: SAConn):
-    cursor = await conn.execute(
-        asset.select()
-    )
+    cursor = await conn.execute(str(Query(Asset).as_scalar()))
     result = await cursor.fetchall()
     return result
 
 
 async def add_asset(conn: SAConn, **asset_data):
     try:
-        await conn.execute(
-            asset.insert().values(**asset_data)
-        )
+        ins = insert(table=Asset).values(**asset_data)
+        await conn.execute(ins)
+    except Exception as ex:
+        logger.debug(ex)
     # TODO catch some exs
-    except:
-        pass
-
-
-async def delete_asset(conn: SAConn, id: int):
-    await conn.execute(
-        asset.delete().where(asset.columns.ticker == id)
-    )
--- models.py
+++ models.py
@@ -1,14 +1,18 @@
 import sqlalchemy as sa
-
+from sqlalchemy.ext.declarative import declarative_base
 from migrations import metadata
 
 from config import sql_conn_url
 
 
+Base = declarative_base()
+
 # Not-async engine for migrations
 sa.engine = sa.create_engine(sql_conn_url, echo=True)
 
-asset = sa.Table(
+
+# TODO remove to migrations
+assets = sa.Table(
     'assets', metadata,
     sa.Column('id', sa.Integer, primary_key=True, index=True),
     sa.Column("ticker", sa.String, unique=True),
@@ -21,3 +25,18 @@ asset = sa.Table(
     sa.Column('maker_fee', sa.Numeric),
     sa.Column("taker_fee", sa.Numeric),
 )
+
+
+class Asset(Base):
+    __tablename__ = "assets"
+
+    id = sa.Column(sa.Integer, primary_key=True, index=True)
+    ticker = sa.Column(sa.String, unique=True)
+    name = sa.Column(sa.String, unique=True)
+    unified_cryptoasset_id = sa.Column(sa.Integer)
+    can_deposit = sa.Column(sa.Boolean)
+    can_withdraw = sa.Column(sa.Boolean)
+    min_withdraw = sa.Column(sa.Numeric)
+    max_withdraw = sa.Column(sa.Numeric)
+    maker_fee = sa.Column(sa.Numeric)
+    taker_fee = sa.Column(sa.Numeric)
