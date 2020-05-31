226960d0c32e115dea9cc45b1fbbc0984ae5bd24
biobdeveloper <ibogomolov321@gmail.com>
2020-06-01 02:55:04 +0300

    Add JSON view decorator syntax

--- views.py
+++ views.py
@@ -10,12 +10,19 @@ from config import PROJECT_NAME, API_V1_ADDRESS
 routes = web.RouteTableDef()
 
 
+def json_view(func):
+    async def wrapped(request):
+        return web.json_response(await func(request))
+    return wrapped
+
+
 @routes.get('/')
 async def index(request):
     return web.Response(text=f"Welcome to {PROJECT_NAME}")
 
 
 @routes.get(f'{API_V1_ADDRESS}/assets/')
+@json_view
 async def assets_view(request):
     async with request.app['db'].acquire() as conn:
         resp_data = await get_all_assets(conn)
@@ -29,15 +36,20 @@ async def assets_view(request):
             except Exception as ex:
                 logging.debug(ex)
 
-        return web.json_response(resp)
+        return resp
 
 
 @routes.get(f'{API_V1_ADDRESS}/coins/')
+@json_view
 async def coins_view(request):
     async with request.app['db'].acquire() as conn:
         resp_data = await get_all_coins(conn)
-        resp = []
+
+        # TODO add many=True arg
         schema = CoinDataSchema()
+
+        # TODO replace to resp = schema.dump(resp_data)
+        resp = []
         for obj in resp_data:
             dict_obj = dict(obj)
             dict_obj["gate_fee"] = obj.withdraw_fee / 10 ** obj.precision
@@ -48,4 +60,4 @@ async def coins_view(request):
             except Exception as ex:
                 logging.debug(ex)
 
-        return web.json_response(resp)
+        return resp
