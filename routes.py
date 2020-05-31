3133cd51f24aa5f25725c5564ade1d1eec2bd160
biobdeveloper <ibogomolov321@gmail.com>
2020-06-01 00:40:31 +0300

    Delete routes file

--- routes.py
+++
@@ -1,8 +0,0 @@
-from views import assets_view, coins_view, index
-from config import API_V1_ADDRESS
-
-
-def init_routes(app) -> None:
-    app.router.add_get('/', index)
-    app.router.add_get(f'{API_V1_ADDRESS}/assets/', assets_view)
-    app.router.add_get(f'{API_V1_ADDRESS}/coins/', coins_view)
